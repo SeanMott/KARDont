c4d94d3ee8cf5708ab5e352628b243f0650c1909
Sean Mott <rafal.mott1@gmail.com>
2024-06-27 15:41:49 -0400

    Tweaked Nintendon't to have custom background and Homebrew info

--- common/include/Metadata.h
+++ common/include/Metadata.h
@@ -2,12 +2,12 @@
 #ifndef __META_DATA_H__
 #define __META_DATA_H__
 
-#define META_NAME			"Nintendont"
-#define META_AUTHOR			"FIX94, crediar"
+#define META_NAME			"KARDont"
+#define META_AUTHOR			"Jas, FIX94, crediar"
 
-#define META_LONG1			"Commiters: GerbilSoft, JoostinOnline, GreyRogue, Howard, Cyan \r\n\r\n Project website: https://github.com/FIX94/Nintendont "
-#define META_LONG2			"Nintendont allows you to run GameCube games on a Wii or Wii U from an SD or HDD device."
-#define META_SHORT			"Gamecube Loader"
+#define META_LONG1			"Based on Nintendon't.\n\nKARDon't Github: \"https://github.com/SeanMott/KARDont\""
+#define META_LONG2			"KARDont allows you to run KAR and it's mods on a Wii or Wii U from an SD or HDD device."
+#define META_SHORT			"KAR Loader"
 
 #define META_XML			"<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"yes\"?>"
 
